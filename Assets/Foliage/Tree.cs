2c76225ed45c83dc119308785cc674ba259f9c22
Kodix86 <cam.peterson@protonmail.com>
2022-05-06 13:16:13 -0500

    Migrate to 3D

--- Assets/Foliage/Tree.cs
+++ Assets/Foliage/Tree.cs
@@ -3,15 +3,15 @@ using System;
 
 public class Tree : Node2D
 {
-	private AnimatedSprite sprites;
+	private AnimatedSprite _sprites;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		sprites = GetNode<AnimatedSprite> ("TreeSprites");
+		_sprites = GetNode<AnimatedSprite> ("TreeSprites");
 	}
 
- // Called every frame. 'delta' is the elapsed time since the previous frame.
-	  public override void _Process(float delta) {
-		  sprites.Frame = 3;
-	  }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta) {
+		_sprites.Frame = (_sprites.Frame + 1) % 4;
+	}
 }
--- Assets/Man/Man.cs
+++ Assets/Man/Man.cs
@@ -1,43 +1,48 @@
 using Godot;
 using System;
+using System.ComponentModel;
 
-public class Man : Node2D {
+public class Man : Spatial {
 	private Random _random = new Random();
-	private Vector2 _bounce = Vector2.Zero;
+	private Vector3 _bounce = Vector3.Zero;
+	private float _speedMod = 1;
 
 	[Export(PropertyHint.Range, "1, 2,")]
-	private float _vStrength = 1.1f;
-	[Export(PropertyHint.Range, "1, 2,")]
-	private float _hStrength = 1.1f;
+	private float _pDecay = 1.2f;
 	[Export(PropertyHint.Range, "-0.5, 0.0,")]
 	private float _maxRotLeft = -0.2f;
 	[Export(PropertyHint.Range, "0, 0.5,")]
 	private float _maxRotRight = 0.2f;
 	[Export(PropertyHint.Range, "0, 1,")]
-	private float _rotSpeed = 0.1f;
+	private float _rotSpeed = 0.3f;
 
-	// public override void _Ready() { }
+	public override void _Ready() {
+		Translation = new Vector3(
+			_random.Next(-12, 12) / 4f,
+			0,
+			_random.Next(-12, 12) / 4f);
 
-	public override void _Process(float delta) {
-		if (!_bounce.IsEqualApprox(Vector2.Zero)) {
-			ContinueBounce();
-			return;
-		}
-
-		_bounce.y = _random.Next() % 32 / 8f - 2f; //NUMBER BETWEEN -2 & 2
-		_bounce.x = _random.Next() % 32 / 8f - 2f; //^^^
-		ContinueBounce();
+		_speedMod +=  _random.Next(64) / 64f - 0.5f;
+		_rotSpeed *= _speedMod;
 	}
 
-	private void ContinueBounce() {
-		Translate(_bounce);
-		_bounce.y /= _vStrength;
-		_bounce.x /= _hStrength;
-		
-		Rotate(_rotSpeed);
+	public override void _Process(float delta) {
+		if(_bounce.IsEqualApprox(Vector3.Zero)) InitNewBounce();
 
-		if (Rotation < _maxRotLeft || Rotation > _maxRotRight) {
-			_rotSpeed *= -1;
-		}
+		RotateZ(_rotSpeed / 10);
+		if (Rotation.z < _maxRotLeft || Rotation.z > _maxRotRight) _rotSpeed *= -1;
+		
+		Translate(_bounce);
+		_bounce /= _pDecay;
+
+		if (Translation.x > 3) Translation = new Vector3(6 - Translation.x, Translation.y, Translation.z);
+		if (Translation.x < -3) Translation = new Vector3(6 + Translation.x, Translation.y, Translation.z);
+		if (Translation.z > 0) Translation = new Vector3(Translation.x, Translation.y, 0);
+		if (Translation.z < -4) Translation = new Vector3(Translation.x, Translation.y, -4);
+	}
+
+	private void InitNewBounce() {
+		_bounce.x = (_random.Next(-32, 32) / 512f) * _speedMod;
+		_bounce.z = (_random.Next(-32, 32) / 512f) * _speedMod;
 	}
 }
